5934bb0046c489cf8b1462d7356359ea1c6042d7
Haydeni0 <hayden.dorahy@gmail.com>
2022-09-11 23:58:34 +0100

    Add parallel execution to txt2img

--- bot_commands.py
+++ bot_commands.py
@@ -11,7 +11,20 @@ def makeBotCommands(bot:commands.bot):
     @bot.command(name="txt2img", help="Generate an image from a prompt (local GPU stable diffusion)")
     async def txt2img(ctx, *, prompt):
         msg = await ctx.send(f"“{prompt}”\n> Generating...")
-        results, time_taken, seeds = make_txt2img(prompt)
+
+        # results, time_taken, seeds = make_txt2img(prompt)
+
+        # >>> use this block to do the txt2img asynchronously in parallel
+        @run_in_executor
+        def do_processing(prompt):
+            return make_txt2img(prompt)
+        async def do_processing_async(prompt):
+            return await do_processing(prompt)
+        tasks = [asyncio.create_task(do_processing_async(prompt))]
+        task_results = [await t for t in asyncio.as_completed(tasks)]
+        results, time_taken, seeds = task_results[0]
+        # <<<
+        
         await msg.edit(content=f"“{prompt}”\n> Done in {time_taken} seconds")
         for img, seed in zip(results, seeds):
             # Save the image in bytes format again
@@ -71,8 +84,8 @@ def makeBotCommands(bot:commands.bot):
             return await blocking(t)
 
         t = float(t)
-        task = [asyncio.create_task(waitblock(t))]
-        [await t for t in asyncio.as_completed(task)]
+        tasks = [asyncio.create_task(waitblock(t))]
+        [await _ for _ in asyncio.as_completed(tasks)]
         await ctx.send(f"Waited {t} seconds")
 
 
