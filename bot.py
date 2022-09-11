4c4a2fae18962609bf69792026e136d20855379e
Haydeni0 <hayden.dorahy@gmail.com>
2022-09-11 21:38:57 +0100

    Add config option for bot command prefix
    This is useful for having different config in a different directory that is used for development.
    
    E.g. use a different command prefix fr the test bot

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 from fileinput import filename
-from turtle import down
 from discord import Intents
 from discord.ext import commands
 from PIL import Image
@@ -19,13 +18,20 @@ config = ConfigParser()
 config.read("config.ini")
 DISCORD_TOKEN = config.get("auth", "DISCORD_TOKEN")
 
+# Define the bot command prefix in the config
+if config.has_option("options", "command_prefix"):
+    command_prefix = config.get("options", "command_prefix")
+else:
+    command_prefix = "/"
+
+print(f"Starting bot using command prefix {command_prefix}")
 
 # Create intents
 intents = Intents.default()
 intents.message_content = True
 # Create discord bot
 bot = commands.Bot(
-    command_prefix="/",
+    command_prefix=command_prefix,
     description="Bot hosted on Hayden's PC (and GPU) \n[https://github.com/Haydeni0/discord-stable-diffusion]",
     intents=intents,
 )
