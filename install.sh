7347dfff1bf23182ffd192f78795e56095dfcd2c
Haydeni0 <hayden.dorahy@gmail.com>
2022-09-15 23:07:42 +0100

    Fix install

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 repo_root_dir=$(dirname "$0")
 
 if { conda env list | grep 'ldm'; } >/dev/null 2>&1; then
-    conda env update -f ${repo_root_dir}/lstein_stable_diffusion/environment.yaml
+    conda env update -f ${repo_root_dir}/lstein_stable_diffusion/environment.yaml --prune
 else
     conda env create -f ${repo_root_dir}/lstein_stable_diffusion/environment.yaml
 fi
@@ -12,7 +12,10 @@ conda init bash
 /bin/bash
 conda activate ldm
 
-python -m pip install py-cord
+if {conda list | grep 'discord'; } >/dev/null 2>&1; then
+    python -m pip uninstall py-cord discord.py 
+fi
+python -m pip install py-cord=2.1.3
 
 python ${repo_root_dir}/lstein_stable_diffusion/scripts/preload_models.py
 
